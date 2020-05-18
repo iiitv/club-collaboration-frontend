1a911e4979f0d22c64de396b2494035928b345da
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-05-19 01:39:22 +0530

    small step for responsive

--- src/main-page/navbar/main navbar/navbar.js
+++ src/main-page/navbar/main navbar/navbar.js
@@ -1,27 +1,32 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link,NavLink} from 'react-router-dom'
 import './navbar.css'
 import menu from './menu.png'
-
+import dot from './3-dot.png'
 
 const Navbar = ()=>{
     return(
-            <div className='navbar-all-links'>
-                <div>
-                    <h1>Club</h1>
-                    <h4>Collaboration</h4>
+            <div className='all-navbar'>
+                <div className='three-dot'>
+                    <img src={dot} alt='main-menu'></img>
                 </div>
-                <div>
-                    <ul className='navbar-center-link'>
-                        <li>Home</li>
-                        <li>Events</li>
-                        <li>Announcement</li>
-                        <li>Login</li>
-                        <li>Our Clubs</li>
-                    </ul>
-                </div>
-                <div >
-                    <Link to='/menu'><img className='slide-menu-icon' alt='menu' src ={menu}></img></Link>
+                <div className='navbar-all-links'>
+                    <div className='logo'>
+                        <h1>Club</h1>
+                        <h4>Collaboration</h4>
+                    </div>
+                    <div className='responsive-menu'>
+                        <ul className='navbar-center-link'>
+                            <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Events'>Events</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Announcement'>Announcement</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Login'>Login</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Clubs'>Our Clubs</NavLink></li>
+                        </ul>
+                    </div>
+                    <div >
+                        <Link to='/menu'><img className='slide-menu-icon' alt='menu' src ={menu}></img></Link>
+                    </div>
                 </div>
              </div>
     )

c973987f5b84f1d868b8169674efef5dfe4250d9
Creator5923 <nlokhande5923@gmail.com>
2020-05-18 21:16:27 -0700

    slideshow completed

--- src/main-page/slideshow/slideshow.js
+++ src/main-page/slideshow/slideshow.js
@@ -7,8 +7,8 @@ import Image from "../../Datafiles/BannerSildeshowImage";
 const Slideshow = () => {
 
     const properties = {
-      duration: 2000,
-      transitionDuration: 1500,
+      duration: 5000,
+      transitionDuration: 1000,
       infinite: true,
       indicators: true,
       arrows: true,
@@ -19,25 +19,14 @@ const Slideshow = () => {
       return (
         <div className="slide-container">
           <Slide  {...properties} className="adjust">
-          
-            {Image.map((article,index)=><div key={index}>
-            
-            <div className="each-slide">
-           
-            <img  className="image-sizing" src={article.name}></img>
-  
-  
+           {Image.map((article,index)=><div key={index}>
+              <div className="each-slide">
+           <img  className="image-sizing" src={article.name}></img>
             </div>
-            
-            </div>
+             </div>
             )}
-  
-  
-  
-          </Slide>
-        </div>
-
-      )
+         </Slide>
+        </div>)
   }
   
   export default Slideshow;

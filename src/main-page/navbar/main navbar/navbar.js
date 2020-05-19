36426f3c52b87f2b75a89fae26fb7219bd1c35b1
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-05-19 09:15:05 +0530

    small change

--- src/main-page/navbar/main navbar/navbar.js
+++ src/main-page/navbar/main navbar/navbar.js
@@ -17,6 +17,11 @@ const Navbar = ()=>{
        }
     }
 
+    const handleMenu = ()=>{
+        var x = document.getElementById('center-link');
+        x.className = 'responsive-menu';
+    }
+
     return(
             <div className='all-navbar'>
                 <div className='three-dot'>
@@ -29,11 +34,11 @@ const Navbar = ()=>{
                     </div>
                     <div className='responsive-menu' id='center-link'>
                         <ul className='navbar-center-link'  >
-                            <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
-                            <li><NavLink exact activeClassName="current" to='/Events'>Events</NavLink></li>
-                            <li><NavLink exact activeClassName="current" to='/Announcement'>Announcement</NavLink></li>
-                            <li><NavLink exact activeClassName="current" to='/Login'>Login</NavLink></li>
-                            <li><NavLink exact activeClassName="current" to='/Clubs'>Our Clubs</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/' onClick={handleMenu}>Home</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Events' onClick={handleMenu}>Events</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Announcement' onClick={handleMenu}>Announcement</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Login' onClick={handleMenu}>Login</NavLink></li>
+                            <li><NavLink exact activeClassName="current" to='/Clubs' onClick={handleMenu}>Our Clubs</NavLink></li>
                         </ul>
                     </div>
                     <div >

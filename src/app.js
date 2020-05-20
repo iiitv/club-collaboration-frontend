57eefbcabf6d19bce9532bf3e3bdfe686e83b618
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-05-20 20:21:01 +0530

    small changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import Navbar from './main-page/navbar/main navbar/navbar'
-import Menubar from './main-page/navbar/second-menu-bar/menu-bar'
 import './index.css'
 import Home from "./main-page/home";
 import Clubmainpage from './component/clubs/club-main-page/club-main-page'
@@ -12,7 +11,6 @@ const App = ()=>{
                <Navbar />
                <Switch>
                    <Route exact path='/' component={Home} />
-                   <Route path='/menu' component={Menubar}/>
                    <Route path='/club/:clubname' component={Clubmainpage} />
                </Switch>
             </div>

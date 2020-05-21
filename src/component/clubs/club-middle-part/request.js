90cc80fec6670a99c9e47cfb19e47e3976de8173
Creator5923 <nlokhande5923@gmail.com>
2020-05-21 08:32:28 -0700

    code beautify

--- src/component/clubs/club-middle-part/request.js
+++ src/component/clubs/club-middle-part/request.js
@@ -1,32 +1,43 @@
-import React from "react"
-import "./request.css"
+import React from "react";
+import "./request.css";
 
-
-const requestJoin=()=>
-{
-    return(
-        <div class="form-container">
-       
-<form autoComplete="off">
-    <input  type="text" id="name" name="name" placeholder="Name" ></input>
-    <br/>   
-    <br/>
-    <input  type="email" id="email" name="email" placeholder="E-mail" ></input>
-    <br/>   
-    <br/>
-    <input type="number" id="batch" name="batch" placeholder="Batch"></input>
-    <br/>   
-    <br/>
-    <br/>
-    <textarea row="10" col="10" id="reason" placeholder="Why do you want to join the club?" autoComplete="off"></textarea>
-</form>
-<br />
-<button type="submit" className="req-btn">Request To Join</button>
-
-     
-        
-        </div>
-    );
-
-}
+const requestJoin = () => {
+  return (
+    <div class="form-container">
+      <form autoComplete="off">
+        <input type="text" id="name" name="name" placeholder="Name"></input>
+        <br />
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="E-mail"
+        ></input>
+        <br />
+        <br />
+        <input
+          type="number"
+          id="batch"
+          name="batch"
+          placeholder="Batch"
+        ></input>
+        <br />
+        <br />
+        <br />
+        <textarea
+          row="10"
+          col="10"
+          id="reason"
+          placeholder="Why do you want to join the club?"
+          autoComplete="off"
+        ></textarea>
+      </form>
+      <br />
+      <button type="submit" className="req-btn">
+        Request To Join
+      </button>
+    </div>
+  );
+};
 export default requestJoin;

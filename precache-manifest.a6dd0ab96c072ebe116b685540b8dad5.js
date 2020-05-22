cb0c06116f690a2fb04a41329deae81c6daf80ea
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-05-22 22:30:59 +0530

    Updates

---
+++ precache-manifest.a6dd0ab96c072ebe116b685540b8dad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e3a981448811807c7ced7aa6b6ac41",
+    "url": "/club-collaboration-frontend/index.html"
+  },
+  {
+    "revision": "69f6282d8c5b6846b90a",
+    "url": "/club-collaboration-frontend/static/css/2.043b003d.chunk.css"
+  },
+  {
+    "revision": "bec659e6efe7d8df41ce",
+    "url": "/club-collaboration-frontend/static/css/main.fdcc8eb8.chunk.css"
+  },
+  {
+    "revision": "69f6282d8c5b6846b90a",
+    "url": "/club-collaboration-frontend/static/js/2.dc954d04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/club-collaboration-frontend/static/js/2.dc954d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec659e6efe7d8df41ce",
+    "url": "/club-collaboration-frontend/static/js/main.c5b84d45.chunk.js"
+  },
+  {
+    "revision": "9e7d5e0cd20122a097dc",
+    "url": "/club-collaboration-frontend/static/js/runtime-main.35a0f001.js"
+  }
+]);

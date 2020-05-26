b081fa78e2a770e539669244400c23b7bec7523a
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-05-26 23:40:35 +0530

    minute changes

--- src/component/clubs/club-main-page/club-main-page.js
+++ src/component/clubs/club-main-page/club-main-page.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 import Clubleft from '../club-left-part/club-left-part'
 import Clubright from '../club-right-part/club-right'
 import './club-main-page.css'
-import {Link,BrowserRouter as Router ,Route,Switch} from "react-router-dom"
+import {BrowserRouter as Router ,Route,Switch} from "react-router-dom"
 import Request from "../club-middle-part/request"
 class Clubmainpage extends Component{
     render(){

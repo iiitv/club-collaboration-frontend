a5d3810a1d9b24b5ffd01eb4505041a68f54fdea
shravanikaware <shravanikaware12@gmail.com>
2022-10-28 20:49:20 +0530

    created login page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import './index.css'
 import Home from "./main-page/home";
 import Clubmainpage from './component/clubs/club-main-page/club-main-page'
 import Eventss from './Eventss';
+import LoginM from './LoginM';
 // import Events from './main-page/Events/Events';
 const App = ()=>{
     return(
@@ -14,6 +15,7 @@ const App = ()=>{
                <Switch>
                    <Route exact path='/' component={Home}  />
                    <Route exact path='/Eventss' component={Eventss} />
+                   <Route exact path='/LoginM' component={LoginM} />
                    {/* <Route exact path='/Events' component={Events} /> */}
                    <Route path='/club/:clubname' component={Clubmainpage} />
                </Switch>
---
+++ src/main-page/Login.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react';
+import "./login.css"
+
+export const Login = () => {
+
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const [allEntry,setAllEntry]=useState([]);
+
+
+    const submitform=()=>{
+        const newentry={email:email , password:password};
+        setAllEntry([...allEntry,newentry]);
+        console.log(allEntry);
+        }
+      return (
+    <>
+    <div className="containerr">
+
+    
+    <form action="" onSubmit={submitform} className="form">
+<div>
+    <label htmlFor="email">Email </label>
+    <input type="text" name="email" id="email"  autoComplete='off' value={email}
+    onChange={(e)=>setEmail(e.target.value)}
+    />
+   
+</div>
+<div>
+    <label htmlFor="password">password </label>
+    <input type="text" name="password" id="password"  autoComplete='off' value={password}
+    onChange={(e)=>setPassword(e.target.value)}
+    />
+   
+</div>
+<button type='submit'>Login</button>
+
+    </form>
+    </div>
+    </>
+  )
+}

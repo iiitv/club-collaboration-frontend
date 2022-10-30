97e038ed68ed76d58e6755a603f3c011db716123
shravanikaware <shravanikaware12@gmail.com>
2022-10-30 12:11:08 +0530

    added clubs data

--- src/Datafiles/clubs.js
+++ src/Datafiles/clubs.js
@@ -1,14 +1,14 @@
 const clubsdata = {
     club : [
-        {id:1,image:'Images/clublogo/coding.jpeg',club:'Coding',sirname:'Club',sec:'ABC',jsec:'DEF'},
-        {id:2,image:'Images/clublogo/shades.jpeg',club:'Shades',sirname:'',sec:'ABC',jsec:'DEF'},
-        {id:3,image:'Images/clublogo/masquerade.jpeg',club:'Masquerade',sirname:'',sec:'ABC',jsec:'DEF'},
-        {id:4,image:'Images/clublogo/mod5.jpeg',club:'Mod5',sirname:'',sec:'ABC',jsec:'DEF'},
-        {id:5,image:'Images/clublogo/encore.jpeg',club:'Encore',sirname:'',sec:'ABC',jsec:'DEF'},
-        {id:6,image:'Images/clublogo/arcadia.jpg',club:'Arcadia',sirname:'',sec:'ABC',jsec:'DEF'},
-        {id:7,image:'Images/clublogo/obscura.jpeg',club:'Obscura',sirname:'',sec:'ABC',jsec:'DEF'},
-        {id:8,image:'Images/clublogo/literature.jpeg',club:'Literature',sirname:'Club',sec:'ABC',jsec:'DEF'},
-        {id:9,image:'Images/clublogo/dot.jpeg',club:'Dot',sirname:'Club',sec:'ABC',jsec:'DEF'}
+        {id:1,image:'Images/clublogo/coding.jpeg',club:'Coding',sirname:'Club',sec:'Mugdha Sharma',jsec:'Aniruddh Modi'},
+        {id:2,image:'Images/clublogo/shades.jpeg',club:'Shades',sirname:'',sec:'Vranda Mahajan',jsec:'DEF'},
+        {id:3,image:'Images/clublogo/masquerade.jpeg',club:'Masquerade',sirname:'',sec:'Aradhya Mishra , Rajan Kumar Panjira',jsec:'Chavvi Gupta'},
+        {id:4,image:'Images/clublogo/mod5.jpeg',club:'Mod5',sirname:'',sec:'Anisha Katiyar',jsec:'Arya Bhatt'},
+        {id:5,image:'Images/clublogo/encore.jpeg',club:'Encore',sirname:'',sec:'Suryansh Shukla',jsec:'Ansh Zala'},
+        {id:6,image:'Images/clublogo/arcadia.jpg',club:'Arcadia',sirname:'',sec:'Sahil Kumar Rai',jsec:'Shubham Mehatre'},
+        {id:7,image:'Images/clublogo/obscura.jpeg',club:'Obscura',sirname:'',sec:'Chetan Verma',jsec:'Nishant Dev'},
+        {id:8,image:'Images/clublogo/literature.jpeg',club:'Literature',sirname:'Club',sec:'Tejas Joshi',jsec:'Dhruv Sharma'},
+        {id:9,image:'Images/clublogo/dot.jpeg',club:'Dot',sirname:'Club',sec:'Suyash Jain',jsec:'Sanidhya Kumar '}
 
     ]
 }

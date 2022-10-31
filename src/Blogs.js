7db51a8afbf7c4f32598b3ca7987485c0f3e3d2f
shravanikaware <shravanikaware12@gmail.com>
2022-10-31 18:15:15 +0530

    added blogs page

---
+++ src/Blogs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Blog } from './main-page/Blog'
+
+export const Blogs = () => {
+  return (
+    <div>
+        <Blog/>
+        <Blog/>
+        <Blog/>
+        <Blog/>
+    </div>
+  )
+}
